66e15aa6d42b0a645c076e7819a76804df8723e1
tuteng <eguangning@gmail.com>
2019-01-19 21:18:19 +0800

    format code

--- pulsar/config_test.go
+++ pulsar/config_test.go
@@ -1,3 +1,20 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package pulsar
 
 import (
@@ -6,17 +23,17 @@ import (
 
 func Test_pulsarConfig_Validate(t *testing.T) {
 	type fields struct {
-		URL                                string
-		UseTls                             bool
-		TLSTrustCertsFilePath              string
-		Topic                              string
+		URL                   string
+		UseTls                bool
+		TLSTrustCertsFilePath string
+		Topic                 string
 	}
 	tests := []struct {
 		name    string
 		fields  fields
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 		{
 			"test url",
 			fields{
@@ -51,10 +68,10 @@ func Test_pulsarConfig_Validate(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &pulsarConfig{
-				URL:                      tt.fields.URL,
-				UseTls:                     tt.fields.UseTls,
-				TLSTrustCertsFilePath:      tt.fields.TLSTrustCertsFilePath,
-				Topic:                              tt.fields.Topic,
+				URL:                   tt.fields.URL,
+				UseTls:                tt.fields.UseTls,
+				TLSTrustCertsFilePath: tt.fields.TLSTrustCertsFilePath,
+				Topic:                 tt.fields.Topic,
 			}
 			if err := c.Validate(); (err != nil) != tt.wantErr {
 				t.Errorf("pulsarConfig.Validate() error = %v, wantErr=%v", err, tt.wantErr)
